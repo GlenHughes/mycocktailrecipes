f1b1e99f09616824cef0b4cfe8c5e6ec9a616a47
GlenHughes <glen.866@gmail.com>
2023-03-19 14:26:13 +0000

    Update copy and styling

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,74 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import Head from "next/head"
+import { useState } from "react"
+import styles from "./index.module.css"
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [cocktailInput, setCocktailInput] = useState("")
+  const [result, setResult] = useState()
 
   async function onSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
     try {
+      const cocktailSearch = cocktailInput
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
-      });
+        body: JSON.stringify({ cocktail: cocktailSearch }),
+      })
 
-      const data = await response.json();
+      const data = await response.json()
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw data.error || new Error(`Request failed with status ${response.status}`)
       }
-
-      setResult(data.result);
-      setAnimalInput("");
+      setResult(data.result)
+      // setCocktailInput()
     } catch(error) {
       // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
+      console.error(error)
+      alert(error.message)
     }
+  }
+
+  function displayResult(result) {
+    if (!result) return ""
+    if (typeof result === "string") {
+      result = result.split("\n")
+
+    }
+    if (!result) return "<li>500 error</li>"
+    console.log(result)
+    const cocktailsAsResponse = result.shift() || result
+    return cocktailsAsResponse.map((r) => {
+      return <li>{r.replace("-", "")}</li>
+    })
   }
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>MyCocktailRecipe</title>
+        {/* <link rel="icon" href="/dog.png" /> */}
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h1>MyCocktailRecipes.com</h1>
+        <p>Enter cocktail name for a recipe or ingredients for ideas</p>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="cocktail"
+            placeholder="i.e. Mojito, Sex on the beach or Peach, Vodka"
+            value={cocktailInput}
+            onChange={(e) => setCocktailInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <div className={styles.result}>
+            <ul>{displayResult(result)}</ul>
+        </div>
       </main>
     </div>
-  );
+  )
 }
