50be5c49686b733e9a8b62d6b28bd370d6b02367
Glen Hughes <glen.866@gmail.com>
2023-05-07 17:02:33 +0100

    Convert to esmodule

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-module.exports = {
-  webpack: (config) => {
-    // this will override the experiments
-    config.experiments = { ...config.experiments, topLevelAwait: true }
-    // this will just update topLevelAwait property of config.experiments
-    // config.experiments.topLevelAwait = true 
-    return config
-  },
+export function webpack(config) {
+  // this will override the experiments
+  config.experiments = { ...config.experiments, topLevelAwait: true }
+  // this will just update topLevelAwait property of config.experiments
+  // config.experiments.topLevelAwait = true 
+  return config
 }
